3f8ee69b8ddb2febe3de0a27832397b77c983a4f
AkashRamlal1 <90188404+AkashRamlal1@users.noreply.github.com>
2022-08-29 11:58:55 +0200

    Add files via upload

---
+++ portfolio-function.js
@@ -0,0 +1,42 @@
+function htmlbar() {
+    document.getElementById("progress-title").innerHTML = "HTML";
+    document.getElementById("progresscolour").style.backgroundColor = "#FF8A00  ";
+    document.getElementById("progresscolour").style.borderRadius = "3rem";
+    document.getElementById("progresscolour").style.width = "65%"
+    document.getElementById("progresscolour").innerHTML = "65%";
+   
+}
+
+function cssbar() {
+    document.getElementById("progress-title").innerHTML = "CSS";
+    document.getElementById("progresscolour").style.backgroundColor = "#00a0f0";
+    document.getElementById("progresscolour").style.borderRadius = "3rem";
+    document.getElementById("progresscolour").style.width = "75%"
+    document.getElementById("progresscolour").innerHTML = "75%";
+}
+
+function phpbar() {
+    document.getElementById("progress-title").innerHTML = "PHP";
+    document.getElementById("progresscolour").style.backgroundColor = "#FFD804";
+    document.getElementById("progresscolour").style.borderRadius = "3rem";
+    document.getElementById("progresscolour").style.width = "30%"
+    document.getElementById("progresscolour").innerHTML = "30%";
+}
+
+function javascriptbar() {
+    document.getElementById("progress-title").innerHTML = "javascript";
+    document.getElementById("progresscolour").style.backgroundColor = "#73FFFC";
+    document.getElementById("progresscolour").style.borderRadius = "3rem";
+    document.getElementById("progresscolour").style.width = "40%"
+    document.getElementById("progresscolour").innerHTML = "40%";
+   
+}
+
+function pythonbar() {
+    document.getElementById("progress-title").innerHTML = "python";
+    document.getElementById("progresscolour").style.backgroundColor = "#F4FF4F";
+    document.getElementById("progresscolour").style.borderRadius = "3rem";
+    document.getElementById("progresscolour").style.width = "60%"
+    document.getElementById("progresscolour").innerHTML = "60%";
+   
+}
